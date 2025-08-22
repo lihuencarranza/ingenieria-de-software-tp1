3ea82f747e56f38650c6d1b1ae36453362d315ed
Lihuén Carranza <lcarranza@fi.uba.ar>
2025-08-22 14:28:38 -0400

    add: main to start the server

--- cmd/main.go
+++ cmd/main.go
@@ -1,6 +1,20 @@
 package main
 
+import (
+	"log"
+	"os"
+
+	"melodia/internal/server"
+
+	"github.com/joho/godotenv"
+)
 
 func main() {
-	
+	err := godotenv.Load()
+	if err != nil {
+		log.Println("Unable to load file .env")
+		os.Exit(1)
+	}
+
+	server.Start()
 }
