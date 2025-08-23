5069b43a4bffcba5d61b2453037e375cdf1fb424
Lihuén Carranza <lcarranza@fi.uba.ar>
2025-08-23 14:58:37 -0400

    add: error model struct

---
+++ internal/models/error.go
@@ -0,0 +1,10 @@
+package models
+
+// ErrorResponse represents an error response following RFC 7807
+type ErrorResponse struct {
+	Type     string `json:"type"`
+	Title    string `json:"title"`
+	Status   int    `json:"status"`
+	Detail   string `json:"detail"`
+	Instance string `json:"instance"`
+}
